7bf3f95d14a7d52a47ad782ea692c29ade210ba2
Your Name <you@example.com>
2023-01-08 20:00:39 +0100

    add /

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { ChakraProvider } from '@chakra-ui/react'
-import { HashRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -10,9 +10,9 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <ChakraProvider>
-    <HashRouter>
+    <BrowserRouter basename='/calc'>
     <App />
-    </HashRouter>
+    </BrowserRouter>
     </ChakraProvider>
 
   </React.StrictMode>
